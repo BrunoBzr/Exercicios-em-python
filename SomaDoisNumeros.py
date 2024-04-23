e0730aae02194c637fe7ef35d62a6f478ad646bd
Bruno Pereira <brunopereirabzrdev@gmail.com>
2024-04-23 20:55:05 -0300

    Soma de dois numeros inteiros
    Leia dois valores inteiro e efetue a soma deles, então mostre o resultado da soma.

---
+++ SomaDoisNumeros.py
@@ -0,0 +1,6 @@
+primeiro_numero = int(input('Informe o primeiro numero: '))
+segundo_numero = int(input('Informe o segundo numero: '))
+
+soma = primeiro_numero + segundo_numero
+
+print('A soma de {} + {} é igual a {}'.format(primeiro_numero, segundo_numero, soma))
