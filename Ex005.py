2df10310bbad3794aa28c781d9e823783d831973
Bruno Pereira <brunopereirabzrdev@gmail.com>
2024-06-26 04:35:03 -0300

    Update Ex005.py

--- Ex005.py
+++ Ex005.py
@@ -1,14 +1,3 @@
-"""
-Ex005: Faça um programa que leia as duas notas de um aluno em uma matéria e mostre na tela a sua média na disciplina.Ex:
-    Entrada:
-        Nota 1: 4.5
-        Nota 2: 8.5
-    Saída:
-        Primeira nota: 4.5
-        Segunda nota: 8.5
-        Média: 6.5
-"""
-
 primeira_nota = float(input('Informe a primeira nota do aluno: '))
 segunda_nota = float(input('Informe a segunda nota do aluno: '))
 
